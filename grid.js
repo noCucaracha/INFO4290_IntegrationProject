74c4d01863dfd711a122fbece7f1bdfbb1144f93
noCucaracha <120060763+noCucaracha@users.noreply.github.com>
2023-02-17 18:45:35 -0800

    1.1
    implemented responsive media control

--- grid.js
+++ grid.js
@@ -18,32 +18,39 @@ function gridGen(){
 
     var gheight=document.getElementById("container").offsetHeight;
     var gwidth=document.getElementById("container").offsetWidth;
-    var bheight=Math.ceil(gheight/30);
-    var bwidth=Math.ceil(gwidth/30);
+    var bheight=Math.ceil(gheight/25);
+    var bwidth=Math.ceil(gwidth/25);
     let hasStart = false, hasFinish = false;
     let rand1 = Math.abs(getRandInt(bheight));
     let rand2 = Math.abs(getRandInt(bwidth));
     let rand3 = Math.abs(getRandInt(bheight));
     let rand4 = Math.abs(getRandInt(bwidth));
+    
 
     let tableHTML ="<tbody>";
     for(let row=0; row<bheight; row++){
         let currentRow = [];
        let currentRowHTML=`<tr id="row${row}">`;
         for(let column = 0; column<bwidth; column++){
+            let nodeContent = " ";
             let nodeID = `${row}-${column}`, nodeState="node";
             let newNode = new Node(nodeID, nodeState, row, column);
-            if(row===rand1&&column===rand2&&hasStart===false&&nodeState!="Finish"){
-                nodeState="Start";
+            if(row===rand1&&column===rand2&&hasStart===false&&nodeState!="nodeFinish"){
+                nodeState="nodeStart";
                 hasStart=true;
+                nodeContent = "S";
+                
             }
-            else if (row===rand3&&column===rand4&&hasFinish===false&&nodeState!="Start"){
-                nodeState="Finish";
+            else if (row===rand3&&column===rand4&&hasFinish===false&&nodeState!="nodeStart"){
+                nodeState="nodeFinish";
                 hasFinish=true; 
+                nodeContent = "F";
+
+            
             }
             
             currentRow.push(newNode);
-            currentRowHTML+=`<td id="row${row}_column${column}" class="${nodeState}"></td>`;
+            currentRowHTML+=`<td id="row${row}_column${column}" class="${nodeState}">${nodeContent}</td>`;
         }
         
         tableHTML+=`${currentRowHTML}</tr>`;
@@ -56,4 +63,6 @@ function gridGen(){
     mygrid.innerHTML=tableHTML;
 }
 
+
 gridGen();
+
